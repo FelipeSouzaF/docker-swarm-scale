0aea942eedcdff0e8a56a88b9b9c7c99aa8c1414
Felipe Farias <felipesouzaf11@gmail.com>
2023-06-10 15:35:33 -0300

    feat: add fibonacci function

--- files/stackdemo/app.py
+++ files/stackdemo/app.py
@@ -4,10 +4,28 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+def fibonacci(n):
+    if n <= 0:
+        return []
+
+    sequence = [0, 1]
+
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence
+
+# Example usage
+n = 1000
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+    result = fibonacci(n)
+
+    return 'Hello World! I have been seen {} times. {} \n'.format(count, result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
